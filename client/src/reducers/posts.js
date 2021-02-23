17e297455d0dcf5b09d28d5115ba6eaa1aac9aac
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-23 23:58:02 +0100

    Just a comment

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,8 +3,10 @@ export default (posts = [], action) => {
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
-            return posts;    
+            return [...posts, action.payload];    
         default:
             return posts;
     }
 }
+
+//Nothing much
